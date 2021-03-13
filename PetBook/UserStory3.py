615cdba83bb99688aa8e6e3b5c5778cf49c41d10
PAsher2016 <77692371+PAsher2016@users.noreply.github.com>
2021-03-12 20:51:33 -0600

    Update UserStory3.py

--- PetBook/UserStory3.py
+++ PetBook/UserStory3.py
@@ -3,8 +3,20 @@
 import os
 from flask import Flask, render_template, url_for, redirect
 from flask import request, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+class Comment(db.Model):
+    
+    __tablename__ = 'User Comments'
+    id = database.Column(db.Integer, db.ForeignKey('owners'))
+    pet_id = database.Column(db.Integer, db.ForeignKey('pets'))
+    text = database.Column(db.String(200), primary_key = True)
 
 # Insert comment function
 @app.route('/comment', methods = ['GET', 'POST'])
