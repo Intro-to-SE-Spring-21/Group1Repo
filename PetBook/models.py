39c713ab1af6a58d0d714e3a53fde839eff6dae7
Nishan191 <nishankarki191@gmail.com>
2021-03-17 02:28:45 -0500

    Add files via upload
    This file contains all the important models for classes.

---
+++ PetBook/models.py
@@ -0,0 +1,68 @@
+"""
+Reference:
+This project was done by completing the tutorial of Jose Portilla Flask Bootcamp(udemy). I have used the very similiar ideas, format, and code structure used in his tutorial to work on my project.
+
+"""
+
+from PetBook import db,login_manager
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+# The user_loader decorator allows flask-login to load the current user
+# and grab their id.
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+class User(db.Model, UserMixin):
+    #User Account Model
+
+    # Create a table in the db
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    profile_image = db.Column(db.String(20), nullable=False, default='default_profile.png')
+    email = db.Column(db.String(64), unique=True, index=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    # This connects PetPosts to a User Author.
+    posts = db.relationship('PetPost', backref='author', lazy=True)
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self,password):
+        # https://stackoverflow.com/questions/23432478/flask-generate-password-hash-not-constant-output
+        return check_password_hash(self.password_hash,password)
+
+    def __repr__(self):
+        return f"UserName: {self.username}"
+
+class PetPost(db.Model):
+    # Setup the relationship to the User table
+    users = db.relationship(User)
+
+    # Model for the Pet Posts on Website
+    id = db.Column(db.Integer, primary_key=True)
+    # Notice how we connect the PetPost to a particular author
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(140), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+
+
+
+    def __init__(self, title, text, user_id):
+        self.title = title
+        self.text = text
+        self.user_id =user_id
+
+
+    def __repr__(self):
+        return f"Post Id: {self.id} --- Date: {self.date} --- Title: {self.title}"
